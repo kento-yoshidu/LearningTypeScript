9dea7e86413426f7f0d926900760f9c3e7e8d014
kento <kento@toriwatari.bird>
2021-11-08 21:17:30 +0900

    ✨ Update

---
+++ src/overload/script.ts
@@ -0,0 +1,18 @@
+type Reserve = {
+  (from: Date, to: Date, destination: string): void
+  (from: Date, destination: string)
+}
+
+let reserve: Reserve = (
+  from: Date,
+  toOrDestination: Date | string,
+  destination?: string
+): void => {
+  if (toOrDestination instanceof Date && destination !== undefined) {
+    console.log(`${from}, ${destination}`)
+  } else if (typeof toOrDestination === 'string') {
+    console.log(`${from}, ${toOrDestination}, ${destination}`)
+  }
+}
+
+reserve(new Date(), new Date(), "london")
