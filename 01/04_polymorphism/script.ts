7d2703d53c64dec4e3c19528a3af00c87f3b8066
kento <kento@toriwatari.bird>
2021-11-14 20:56:56 +0900

    ✨ Update

--- 01/04_polymorphism/script.ts
+++ 01/04_polymorphism/script.ts
@@ -15,3 +15,29 @@ const filter: Filter = (array, f) => {
 }
 
 console.log(filter([1, 2, 3, 4, 5], _ => _ < 3))
+// [1, 2]
+
+console.log(filter(['a', 'b', 'c'], _ => _ === 'a'))
+// ['a']
+
+const names = [
+  { firstName: "kento" },
+  { firstName: "hiroshi" },
+  { firstName: "keiko"},
+]
+
+console.log(filter(names, _ => _.firstName.startsWith('k')))
+// [ { firstName: 'kento' }, { firstName: 'keiko' } ]
+
+// 型エイリアスにTのスコープを設定
+
+type Sample<T> = {
+  (arg: T): T
+}
+
+const sample: Sample<string> = (str) => {
+  return str
+}
+
+console.log(sample('Hello World'))
+// Hello World
