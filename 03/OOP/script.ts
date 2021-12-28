0834f6526d42d6050e92557c7d9b12a0c21bc722
kento <kento@toriwatari.bird>
2021-12-28 22:17:25 +0900

    ✨ Update

--- 03/OOP/script.ts
+++ 03/OOP/script.ts
@@ -113,7 +113,6 @@ const p: SubPerson = new SubPerson("Kento")
 console.log(p.getName())
 //=> Kento
 
-*/
 
 class Person {
   private name: string
@@ -135,5 +134,138 @@ const p: Person = new Person("Kento")
 console.log(p.getName())
 //=> Kento
 
+// constructor shorthand
 
+class Test {
+  constructor(
+    arg0: number,
+    public arg1: number,
+    protected arg2: number,
+    private arg3: number,
+  ) {
+    console.log({ arg0, arg1, arg2, arg3 })
+  }
+}
 
+const t = new Test(0, 1, 2, 3)
+//=> { arg0: 0, arg1: 1, arg2: 2, arg3: 3 }
+
+// cannot access
+//console.log(t.arg0)
+// error TS2339: Property 'arg0' does not exist on type 'Test'.
+
+// can acess
+// console.log(t.arg1)
+//=> 1
+
+// cannot access
+// console.log(t.arg2)
+// error TS2445: Property 'arg2' is protected and only accessible within class 'Test' and its subclasses.
+
+// cannot acess
+// console.log(t.arg3)
+// error TS2341: Property 'arg3' is private and only accessible within class 'Test'.
+
+// 読み取り専用フィールド
+
+class Person {
+  readonly name: string
+  readonly age: number
+
+  constructor(name: string, age: number) {
+    this.name = name
+    this.age = age
+  }
+
+  setProperty(name: string, age: number): void {
+    this.name = name
+    // error TS2540: Cannot assign to 'name' because it is a read-only property.
+    this.age = age
+    // error TS2540: Cannot assign to 'age' because it is a read-only property.
+  }
+}
+
+// 静的プロパティ
+
+class Person {
+  static str: string = "World"
+
+  static sayHello() {
+    console.log(`Hello ${this.str}`)
+  }
+}
+
+Person.sayHello()
+//=> Hello World
+
+class Operator {
+  protected value: number
+
+  constructor(value: number) {
+    this.value = value
+  }
+
+  sum(value: number): void {
+    this.value += value
+  }
+  subtract(value: number): void {
+    this.value -= value
+  }
+  multiply(value: number): void {
+    this.value *= value
+  }
+  devide(value: number): void {
+    this.value /= value
+  }
+
+  getValue() {
+    return this.value
+  }
+}
+
+const p = new Operator(10)
+
+p.sum(20)
+console.log(p.getValue())
+//=> 30
+
+p.subtract(3)
+console.log(p.getValue())
+//=> 27
+
+p.multiply(10)
+console.log(p.getValue())
+//=> 270
+
+p.devide(3)
+console.log(p.getValue())
+//=> 90
+
+*/
+
+class Operator {
+  protected value: number
+
+  constructor(value: number) {
+    this.value = value
+  }
+
+  sum(value: number): void {
+    this.value += value
+  }
+  subtract(value: number): void {
+    this.value -= value
+  }
+  multiply(value: number): void {
+    this.value *= value
+  }
+  devide(value: number): void {
+    this.value /= value
+  }
+
+  getValue() {
+    return this.value
+  }
+}
+
+const p = new Operator(10)
